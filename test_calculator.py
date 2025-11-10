492efa8876f953e83f30d05a317bc2559c63be05
elijah baez <elijahbaez070@gmail.com>
2025-11-10 10:23:53 -0500

    p1tests

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,9 @@
+# https://github.com/elijahbaez/lab10-EB-CW.git
+# Partner 1: Elijah Baez
+# Partner 2: Curtis Whipple
+
 import unittest
-from calculator import *
+import calculator
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
@@ -11,12 +15,15 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self):
+        self.assertEqual(calculator.mul(3, 4), 12)
+        self.assertEqual(calculator.mul(-2, 5), -10)
+        self.assertEqual(calculator.mul(0, 100), 0)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self):
+        self.assertEqual(calculator.div(2, 10), 5)
+        self.assertEqual(calculator.div(5, 10), 2)
+        self.assertAlmostEqual(calculator.div(4, 1), 0.25)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -34,22 +41,25 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            calculator.log(-10, 100)  # Negative base
+        with self.assertRaises(ValueError):
+            calculator.log(10, -100)  # Negative number
+        with self.assertRaises(ValueError):
+            calculator.log(1, 100)  # Base of 1
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self):
+        self.assertEqual(calculator.hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(calculator.hypotenuse(5, 12), 13)
+        self.assertEqual(calculator.hypotenuse(-3, 4), 5)  # Works with negative nums
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_sqrt(self):
+        self.assertEqual(calculator.square_root(16), 4)
+        self.assertAlmostEqual(calculator.square_root(2), 1.41421356)
+        with self.assertRaises(ValueError):
+            calculator.square_root(-9)
+
 
 # Do not touch this
 if __name__ == "__main__":
